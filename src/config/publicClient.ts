e4df23e58ff20a2573ca61b081ea883cdf2d4092
Alexey Gelich <alexeygelich@gmail.com>
2022-05-25 13:19:16 +0300

    update baseURL

--- src/config/publicClient.ts
+++ src/config/publicClient.ts
@@ -2,4 +2,5 @@ import axios from 'axios';
 
 export const publicClient = axios.create();
 
-publicClient.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+// publicClient.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+publicClient.defaults.baseURL = 'https://api.github.com';
